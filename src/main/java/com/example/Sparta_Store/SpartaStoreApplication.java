1ca8e526ef327ac4b7e91900bc4fe4f15b7be330
Groo <woohyeon3382@naver.com>
2025-02-10 16:06:53 +0900

    feat: test commit

---
+++ src/main/java/com/example/Sparta_Store/SpartaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sparta_Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpartaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpartaStoreApplication.class, args);
+	}
+
+}
