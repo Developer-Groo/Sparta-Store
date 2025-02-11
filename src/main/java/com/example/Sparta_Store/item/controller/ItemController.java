3753ad42de121d70bc4dc540df3f5a1cbc56a844
Groo <woohyeon3382@naver.com>
2025-02-11 17:10:42 +0900

    feat: ItemController 구현

--- src/main/java/com/example/Sparta_Store/item/controller/ItemController.java
+++ src/main/java/com/example/Sparta_Store/item/controller/ItemController.java
@@ -1,10 +1,32 @@
 package com.example.Sparta_Store.item.controller;
 
+import com.example.Sparta_Store.item.dto.response.ItemResponseDto;
+import com.example.Sparta_Store.item.service.ItemService;
+import com.example.Sparta_Store.util.PageQuery;
+import com.example.Sparta_Store.util.PageResult;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/items")
 public class ItemController {
 
+    private final ItemService itemService;
+
+    @GetMapping
+    public ResponseEntity<PageResult<ItemResponseDto>> getItems(PageQuery pageQuery) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(itemService.getItems(pageQuery));
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<PageResult<ItemResponseDto>> getSearchItems(String keyword, PageQuery pageQuery) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(itemService.getSearchItems(keyword, pageQuery));
+    }
 }
