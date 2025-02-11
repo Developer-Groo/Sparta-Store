60905b455673a98be8da61eeed81343030956454
Groo <woohyeon3382@naver.com>
2025-02-11 17:10:29 +0900

    feat: ItemService 구현

--- src/main/java/com/example/Sparta_Store/item/service/ItemService.java
+++ src/main/java/com/example/Sparta_Store/item/service/ItemService.java
@@ -1,8 +1,34 @@
 package com.example.Sparta_Store.item.service;
 
+import com.example.Sparta_Store.item.dto.response.ItemResponseDto;
+import com.example.Sparta_Store.item.repository.ItemQueryRepository;
+import com.example.Sparta_Store.item.repository.ItemRepository;
+import com.example.Sparta_Store.util.PageQuery;
+import com.example.Sparta_Store.util.PageResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ItemService {
 
+    private final ItemRepository itemRepository;
+    private final ItemQueryRepository queryRepository;
+
+    public PageResult<ItemResponseDto> getItems(PageQuery pageQuery) {
+        Page<ItemResponseDto> itemList = itemRepository.findAll(pageQuery.toPageable())
+                .map(ItemResponseDto::toDto);
+
+        return PageResult.from(itemList);
+    }
+
+    public PageResult<ItemResponseDto> getSearchItems(String keyword, PageQuery pageQuery) {
+        Page<ItemResponseDto> itemList = queryRepository.findByName(keyword, pageQuery.toPageable())
+                .map(ItemResponseDto::toDto);
+
+        return PageResult.from(itemList);
+    }
 }
