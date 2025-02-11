36af81ea6464f91e90618bdf5bd37d745cd99593
sy <tndus1886@naver.com>
2025-02-11 20:35:13 +0900

    refactor: 주문 생성 엔드포인트 변경, response body 수정

--- src/main/java/com/example/Sparta_Store/orders/controller/OrderController.java
+++ src/main/java/com/example/Sparta_Store/orders/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.example.Sparta_Store.orders.controller;
 import com.example.Sparta_Store.orders.service.OrderService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("")
+@RequestMapping("/orders")
 public class OrderController {
 
     private final OrderService orderService;
@@ -20,17 +21,17 @@ public class OrderController {
      * 주문 생성 API
      * - 장바구니에서 '결제하기'를 누르면 동작
      */
-    @PostMapping("/carts/checkout")
-    public ResponseEntity<String> createOrder(
+    @PostMapping("/checkout")
+    public ResponseEntity<Map<String,String>> createOrder(
         HttpServletRequest request
     ){
 //        String token = request.getHeader("Authorization");
 
         Long userId = 1L;
         orderService.checkoutCart(userId);
-        return ResponseEntity.status(HttpServletResponse.SC_OK).body("상품 주문이 완료되었습니다.");
+        return ResponseEntity.status(HttpServletResponse.SC_CREATED).body(Map.of("message", "상품 주문이 완료되었습니다."));
     }
-
+    ;
 
     /**
      * 주문 상태 변경 API
