6a81462584a7896f1e7d6c85f3352ac1b14e8da6
sy <tndus1886@naver.com>
2025-02-11 20:10:35 +0900

    feat: 주문 생성 구현

--- src/main/java/com/example/Sparta_Store/OrderItem/service/OrderItemService.java
+++ src/main/java/com/example/Sparta_Store/OrderItem/service/OrderItemService.java
@@ -1,8 +1,37 @@
 package com.example.Sparta_Store.OrderItem.service;
 
+import com.example.Sparta_Store.OrderItem.entity.OrderItem;
+import com.example.Sparta_Store.OrderItem.repository.OrderItemRepository;
+import com.example.Sparta_Store.cartItem.entity.CartItem;
+import com.example.Sparta_Store.orders.entity.Orders;
+import com.example.Sparta_Store.orders.repository.OrdersRepository;
+import jakarta.transaction.Transactional;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class OrderItemService {
+
+    private final OrderItemRepository orderItemRepository;
+    private final OrdersRepository ordersRepository;
+
+    @Transactional
+    public void createOrderItem(Long orderId, List<CartItem> cartItemList){
+        Orders order = ordersRepository.findById(orderId).orElseThrow(
+            () -> new IllegalArgumentException("주문 정보를 찾을 수 없습니다.")
+        );
+        int totalPrice = 0;
+
+        for(CartItem cartItem:cartItemList){
+            int orderPrice = (cartItem.getItem().getPrice())*(cartItem.getQuantity());
+            totalPrice+=orderPrice;
+            OrderItem savedOrderItem = new OrderItem(order, cartItem.getItem(), orderPrice, cartItem.getQuantity());
+            orderItemRepository.save(savedOrderItem);
+        }
+        order.setTotalPrice(totalPrice);
+        ordersRepository.save(order);
+    }
 
 }
--- src/main/java/com/example/Sparta_Store/orders/controller/OrderController.java
+++ src/main/java/com/example/Sparta_Store/orders/controller/OrderController.java
@@ -1,10 +1,47 @@
 package com.example.Sparta_Store.orders.controller;
 
+import com.example.Sparta_Store.orders.service.OrderService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("")
 public class OrderController {
+
+    private final OrderService orderService;
+
+    /**
+     * 주문 생성 API
+     * - 장바구니에서 '결제하기'를 누르면 동작
+     */
+    @PostMapping("/carts/checkout")
+    public ResponseEntity<String> createOrder(
+        HttpServletRequest request
+    ){
+//        String token = request.getHeader("Authorization");
+
+        Long userId = 1L;
+        orderService.checkoutCart(userId);
+        return ResponseEntity.status(HttpServletResponse.SC_OK).body("상품 주문이 완료되었습니다.");
+    }
+
+
+    /**
+     * 주문 상태 변경 API
+     */
+
+    /**
+     * 주문 리스트 조회 API
+     */
+
+    /**
+     * 주문 내역 상세 조회 API
+     */
 
 }
