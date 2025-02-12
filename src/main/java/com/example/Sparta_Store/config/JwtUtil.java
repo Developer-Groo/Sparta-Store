c2a857cc3d9fee3c60624def30c7c51819799f85
ww313 <wlsghddl3358@naver.com>
2025-02-12 10:33:54 +0900

    jwt토í를 읩한 로그인 완료

--- src/main/java/com/example/Sparta_Store/config/JwtUtil.java
+++ src/main/java/com/example/Sparta_Store/config/JwtUtil.java
@@ -14,44 +14,24 @@ import java.util.Date;
 @Slf4j(topic = "JwtUtil")
 @Component
 public class JwtUtil {
-    // JWT 토큰의 접두사
     public static final String BEARER_PREFIX = "Bearer ";
     // JWT 토큰의 만료 시간 일단 60분으로 설정
     private final long TOKEN_TIME = 60 * 60 * 1000L;
-    // JWT 서명 알고리즘
     private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
-    // 애플리케이션 설정 파일에서 주입받은 비밀 키
     @Value("${jwt.secret.key}")
     private String secretKey;
-    // 실제 서명에 사용되는 키 객체
     private Key key;
 
-    /**
-     * 빈 초기화 메서드
-     * - 애플리케이션 시작 시 비밀 키를 Base64로 디코딩하여 Key 객체를 초기화합니다.
-     */
     @PostConstruct
     public void init() {
         byte[] bytes = Base64.getDecoder().decode(secretKey);
         key = Keys.hmacShaKeyFor(bytes);
     }
 
-    /**
-     * JWT 토큰에서 사용자 이름을 추출합니다.
-     * @param token JWT 토큰
-     * @return 사용자 이름
-     */
     public String extractUsername(String token) {
         return extractAllClaims(token).getSubject();
     }
 
-
-
-    /**
-     * JWT 토큰에서 모든 클레임을 추출합니다.
-     * @param token JWT 토큰
-     * @return 클레임 객체
-     */
     private Claims extractAllClaims(String token) {
         return Jwts.parser()
             .setSigningKey(key) // 비밀 키를 사용하여 서명 검증
@@ -59,11 +39,6 @@ public class JwtUtil {
             .getBody();
     }
 
-    /**
-     * JWT 토큰을 생성합니다.
-     * @param email 사용자 이름
-     * @return 생성된 JWT 토큰
-     */
     public String generateToken(String email,String name,Long id) {
         Date date = new Date();
 
@@ -82,32 +57,15 @@ public class JwtUtil {
                 .compact(); // JWT 토큰 생성
     }
 
-    /**
-     * JWT 토큰에서 역할(권한) 정보를 추출합니다.
-     * @param token JWT 토큰
-     * @return 역할 정보 (문자열)
-     */
     public String extractRoles(String token) {
         return extractAllClaims(token).get("auth", String.class);
     }
 
-    /**
-     * JWT 토큰에서 특정 역할이 포함되어 있는지 확인합니다.
-     * @param token JWT 토큰
-     * @param role 확인할 역할
-     * @return 역할 포함 여부 (true: 포함됨, false: 포함되지 않음)
-     */
     public boolean hasRole(String token, String role) {
         String roles = extractRoles(token);
         return roles.contains(role);
     }
 
-    /**
-     * JWT 토큰의 유효성을 검증합니다.
-     *
-     * @param token 검증할 JWT 토큰
-     * @return 토큰의 유효성 여부 (true: 유효함, false: 유효하지 않음)
-     */
     public boolean validateToken(String token) {
         try {
             Jwts.parserBuilder()
