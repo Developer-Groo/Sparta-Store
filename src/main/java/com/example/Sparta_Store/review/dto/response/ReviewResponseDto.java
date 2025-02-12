8201036d2a3028e77b9a126a17df40ad2307496c
Groo <woohyeon3382@naver.com>
2025-02-12 14:18:42 +0900

    feat: ReviewResponseDto 구현

---
+++ src/main/java/com/example/Sparta_Store/review/dto/response/ReviewResponseDto.java
@@ -0,0 +1,24 @@
+package com.example.Sparta_Store.review.dto.response;
+
+import com.example.Sparta_Store.review.entity.Review;
+
+import java.time.LocalDateTime;
+
+public record ReviewResponseDto(
+        Long id,
+        Long userId,
+        String userName,
+        String content,
+        LocalDateTime createdAt
+) {
+
+    public static ReviewResponseDto toDto(Review review) {
+        return new ReviewResponseDto(
+                review.getId(),
+                review.getUser().getId(),
+                review.getUser().getName(),
+                review.getContent(),
+                review.getCreatedAt()
+        );
+    }
+}
