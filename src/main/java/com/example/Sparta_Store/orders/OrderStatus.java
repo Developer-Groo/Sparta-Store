e93eba446e703f851f2bf704cafe9276e9c10893
sy <tndus1886@naver.com>
2025-02-12 10:25:50 +0900

    style: 주문상태 값 영문으로 변환

--- src/main/java/com/example/Sparta_Store/orders/OrderStatus.java
+++ src/main/java/com/example/Sparta_Store/orders/OrderStatus.java
@@ -3,8 +3,17 @@ package com.example.Sparta_Store.orders;
 import java.util.Arrays;
 
 public enum OrderStatus {
-    주문완료, 주문취소요청, 배송준비중, 배송중, 배송완료, 취소완료,
-    반품요청, 교환요청, 반품완료, 교환완료, 구매확정;
+    ORDER_COMPLETED, // 주문완료
+    ORDER_CANCEL_REQUEST, // 주문취소요청
+    PREPARING_SHIPMENT, // 배송준비중
+    SHIPPING, // 배송중
+    DELIVERED, //배송완료
+    CANCELED, //취소완료
+    RETURN_REQUESTED, // 반품요청
+    EXCHANGE_REQUESTED, // 교환요청
+    RETURNED, // 반품완료
+    EXCHANGED, // 교환완료
+    CONFIRMED; // 구매확정
 
     public static OrderStatus of(String orderStatus) {
         return Arrays.stream(OrderStatus.values())
