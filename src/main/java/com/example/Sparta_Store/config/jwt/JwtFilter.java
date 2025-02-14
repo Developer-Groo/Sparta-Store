460cf75cc79ce7ca667e7f5e1cf89ed53f4fda1c
ww313 <wlsghddl3358@naver.com>
2025-02-14 20:49:30 +0900

    fixed for convention

--- src/main/java/com/example/Sparta_Store/config/jwt/JwtFilter.java
+++ src/main/java/com/example/Sparta_Store/config/jwt/JwtFilter.java
@@ -25,9 +25,10 @@ public class JwtFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
     ) throws ServletException, IOException {
         String requestURI = request.getRequestURI();
         String jwt = null;
