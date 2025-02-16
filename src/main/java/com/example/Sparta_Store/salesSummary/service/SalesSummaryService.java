dbbd586bb04cb5772fec768fb444babff00e18c5
Groo <woohyeon3382@naver.com>
2025-02-16 20:27:23 +0900

    feat: SalesSummaryService 구현

---
+++ src/main/java/com/example/Sparta_Store/salesSummary/service/SalesSummaryService.java
@@ -0,0 +1,27 @@
+package com.example.Sparta_Store.salesSummary.service;
+
+import com.example.Sparta_Store.item.entity.Item;
+import com.example.Sparta_Store.salesSummary.entity.SalesSummary;
+import com.example.Sparta_Store.salesSummary.repository.SalesSummaryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class SalesSummaryService {
+
+    private final SalesSummaryRepository repository;
+
+    @Transactional
+    public void updateSales(Item item, int quantity) {
+        SalesSummary summary = repository.findSalesSummaryByItemId(item.getId())
+                .map(existingSummary -> {
+                    existingSummary.incrementSales(quantity);
+                    return existingSummary;
+                })
+                .orElseGet(() -> SalesSummary.createSummary(item, quantity));
+
+        repository.save(summary);
+    }
+}
