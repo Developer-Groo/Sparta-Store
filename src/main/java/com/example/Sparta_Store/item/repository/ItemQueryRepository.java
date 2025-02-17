d983d10e9ca917110eea2b3b9878262a3221b0a0
Groo <woohyeon3382@naver.com>
2025-02-17 10:45:46 +0900

    refactor: inStock 조건 추가 쿼리 메서드 구현 및 수정

--- src/main/java/com/example/Sparta_Store/item/repository/ItemQueryRepository.java
+++ src/main/java/com/example/Sparta_Store/item/repository/ItemQueryRepository.java
@@ -5,5 +5,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface ItemQueryRepository {
-    Page<Item> findByName(String keyword, Pageable pageable);
+
+    Page<Item> findAllByStockCondition(boolean inStock, Pageable pageable);
+
+    Page<Item> findByNameAndStockCondition(boolean inStock, String keyword, Pageable pageable);
+
+    Page<Item> findByCategoryId(Long categoryId, boolean inStock, Pageable pageable);
 }
--- src/main/java/com/example/Sparta_Store/item/repository/ItemQueryRepositoryImpl.java
+++ src/main/java/com/example/Sparta_Store/item/repository/ItemQueryRepositoryImpl.java
@@ -17,16 +17,43 @@ public class ItemQueryRepositoryImpl implements ItemQueryRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Page<Item> findByName(String keyword, Pageable pageable) {
-        JPAQuery<Item> result = queryFactory
+    public Page<Item> findByNameAndStockCondition(boolean inStock, String keyword, Pageable pageable) {
+        JPAQuery<Item> query = queryFactory
                 .selectFrom(item)
-                .where(itemNameLike(keyword));
+                .where(
+                        itemNameLike(keyword),
+                        itemGtZero(inStock)
+                );
 
-        return QuerydslUtil.fetchPage(result, item, pageable);
+        return QuerydslUtil.fetchPage(query, item, pageable);
+    }
+
+    @Override
+    public Page<Item> findAllByStockCondition(boolean inStock, Pageable pageable) {
+        JPAQuery<Item> query = queryFactory
+                .selectFrom(item)
+                .where(itemGtZero(inStock));
+
+        return QuerydslUtil.fetchPage(query, item, pageable);
+    }
+
+    @Override
+    public Page<Item> findByCategoryId(Long categoryId, boolean inStock, Pageable pageable) {
+        JPAQuery<Item> query = queryFactory
+                .selectFrom(item)
+                .where(
+                        item.category.id.eq(categoryId),
+                        itemGtZero(inStock)
+                );
+
+        return QuerydslUtil.fetchPage(query, item, pageable);
     }
 
     private BooleanExpression itemNameLike(String keyword) {
         return keyword != null && !keyword.isEmpty() ? item.name.like("%" + keyword + "%") : null;
     }
 
+    private BooleanExpression itemGtZero(boolean inStock) {
+        return inStock ? item.stockQuantity.gt(0) : null;
+    }
 }
