6a53780232a4fafed71fbc442ed28712da27a712
Groo <woohyeon3382@naver.com>
2025-02-22 02:15:23 +0900

    feat: Lock 메서드, 재고 감소 메서드 추가

--- src/main/java/com/example/Sparta_Store/item/repository/ItemRepository.java
+++ src/main/java/com/example/Sparta_Store/item/repository/ItemRepository.java
@@ -1,7 +1,30 @@
 package com.example.Sparta_Store.item.repository;
 
 import com.example.Sparta_Store.item.entity.Item;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long>, ItemQueryRepository {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query(
+            "select i " +
+                    "from Item i " +
+                    "where i.id in :itemIds"
+    )
+    void findAllByIdWithLock(@Param("itemIds") List<Long> itemIds);
+
+    @Modifying
+    @Query(
+            "update Item i " +
+                    "set i.stockQuantity = i.stockQuantity - :quantity " +
+                    "where i.id = :itemId and i.stockQuantity >= :quantity"
+    )
+    int decreaseStock(@Param("itemId") Long itemId, @Param("quantity") int quantity);
 }
