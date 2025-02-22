b162ed4f865ecfeefb9b43eec446992353729915
Groo <woohyeon3382@naver.com>
2025-02-22 21:03:11 +0900

    feat: Item Error code 정의

--- src/main/java/com/example/Sparta_Store/item/exception/ItemErrorCode.java
+++ src/main/java/com/example/Sparta_Store/item/exception/ItemErrorCode.java
@@ -8,9 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum ItemErrorCode implements ErrorCode {
-    ;
-
-
+    OUT_OF_STOCK(HttpStatus.BAD_REQUEST, "BAD_REQUEST", "재고가 부족합니다.");
 
     private final HttpStatus status;
     private final String name;
