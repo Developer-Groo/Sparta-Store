9d2150f57c64a7eea9ed990b4cd7f8f137f9a1f6
Groo <woohyeon3382@naver.com>
2025-02-22 21:03:26 +0900

    refactor: 사용하지 않는 메서드 제거

--- src/main/java/com/example/Sparta_Store/item/repository/ItemRepository.java
+++ src/main/java/com/example/Sparta_Store/item/repository/ItemRepository.java
@@ -4,7 +4,6 @@ import com.example.Sparta_Store.item.entity.Item;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -18,13 +17,5 @@ public interface ItemRepository extends JpaRepository<Item, Long>, ItemQueryRepo
                     "from Item i " +
                     "where i.id in :itemIds"
     )
-    void findAllByIdWithLock(@Param("itemIds") List<Long> itemIds);
-
-    @Modifying
-    @Query(
-            "update Item i " +
-                    "set i.stockQuantity = i.stockQuantity - :quantity " +
-                    "where i.id = :itemId and i.stockQuantity >= :quantity"
-    )
-    int decreaseStock(@Param("itemId") Long itemId, @Param("quantity") int quantity);
+    List<Item> findAllByIdWithLock(@Param("itemIds") List<Long> itemIds);
 }
