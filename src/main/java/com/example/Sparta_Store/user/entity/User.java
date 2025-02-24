faf5f2b812256cf5f0b059a7d00476df55277653
ww313 <wlsghddl3358@naver.com>
2025-02-24 15:15:32 +0900

    fix : OAUTH 로그인시 customerKey 생성되지않던부분 수정했습니다.

--- src/main/java/com/example/Sparta_Store/user/entity/User.java
+++ src/main/java/com/example/Sparta_Store/user/entity/User.java
@@ -50,7 +50,7 @@ public class User extends TimestampedEntity {
     private UserRoleEnum role;
 
     public User(String email, String password, String name, Address address, UserRoleEnum role) {
-
+        this.isDeleted = false;
         this.email = email;
         this.password = password;
         this.name = name;
@@ -60,11 +60,13 @@ public class User extends TimestampedEntity {
     }
 
     public User(String provider, String providerId , String name, String email, Address address , UserRoleEnum role){
+        this.isDeleted = false;
         this.provider = provider;
         this.providerId = providerId;
         this.name = name;
         this.email = email;
         this.address = address;
+        this.customerKey = UUID.randomUUID().toString();
         this.role = role;
     }
 
