450c2632a3e9f5f67ae78ac73f6cd5ace3bc4aa0
sy <tndus1886@naver.com>
2025-02-25 13:14:14 +0900

    feat: Order Test 추가

---
+++ src/test/java/com/example/Sparta_Store/orders/entity/OrderTest.java
@@ -0,0 +1,51 @@
+package com.example.Sparta_Store.orders.entity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.Sparta_Store.address.entity.Address;
+import com.example.Sparta_Store.oAuth.jwt.UserRoleEnum;
+import com.example.Sparta_Store.orders.OrderStatus;
+import com.example.Sparta_Store.user.entity.Users;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderTest {
+
+    private Users user;
+    private Address address;
+
+    @BeforeEach
+    void setUp() {
+        address = new Address("경기도", "테스트길", "12345");
+        user = new Users(1L, UUID.randomUUID().toString(), "테스트유저", "email@test.com", "Pw1234!!!", address, false, null, null, UserRoleEnum.USER);
+    }
+
+    @Test
+    @DisplayName("주문 생성 성공 - order_status 기본값은 BEFORE_PAYMENT")
+    void createOrder_Default_Order_Status() {
+        // given & when
+        Orders order = new Orders(user, 30000, user.getAddress());
+
+        // then
+        assertThat(order.getOrderStatus().equals(OrderStatus.BEFORE_PAYMENT));
+    }
+
+    @Test
+    @DisplayName("주문상태 변경 성공")
+    void updateOrderStatus() {
+        // given
+        Orders order = new Orders(user, 30000, user.getAddress());
+
+        // when
+        order.updateOrderStatus(OrderStatus.CONFIRMED);
+
+        // then
+        assertThat(order.getOrderStatus().equals(OrderStatus.CONFIRMED));
+    }
+
+}
