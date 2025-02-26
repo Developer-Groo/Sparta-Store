82bd3fe4d9c6364961b3d0a7437166666dfbec8f
Groo <woohyeon3382@naver.com>
2025-02-27 00:43:26 +0900

    feat: Email Event 구현

---
+++ src/main/java/com/example/Sparta_Store/email/service/EmailService.java
@@ -0,0 +1,28 @@
+package com.example.Sparta_Store.email.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Value("${EMAIL_USER_NAME}")
+    private String EMAIL_SENDER;
+
+    public void sendEmail(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setFrom(EMAIL_SENDER);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+
+        mailSender.send(message);
+    }
+}
