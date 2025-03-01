1400395212e0c10fe7b3ed2494985837fbdf1cab
sy <tndus1886@naver.com>
2025-03-01 20:20:15 +0900

    refactor: orderStatus description 추가

--- src/main/java/com/example/Sparta_Store/orders/OrderStatus.java
+++ src/main/java/com/example/Sparta_Store/orders/OrderStatus.java
@@ -2,21 +2,27 @@ package com.example.Sparta_Store.orders;
 
 import java.util.Arrays;
 import java.util.Map;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
 public enum OrderStatus {
-    BEFORE_PAYMENT, // 결제전
-    PAYMENT_CANCELLED, // 결제취소
-    ORDER_COMPLETED, // 주문완료
-    ORDER_CANCEL_REQUEST, // 주문취소요청
-    PREPARING_SHIPMENT, // 배송준비중
-    SHIPPING, // 배송중
-    DELIVERED, //배송완료
-    CANCELED, //취소완료
-    RETURN_REQUESTED, // 반품요청
-    EXCHANGE_REQUESTED, // 교환요청
-    RETURNED, // 반품완료
-    EXCHANGED, // 교환완료
-    CONFIRMED; // 구매확정
+    BEFORE_PAYMENT("결제전"),
+    PAYMENT_CANCELLED("결제취소"),
+    ORDER_COMPLETED("주문완료"),
+    ORDER_CANCEL_REQUEST("주문취소요청"),
+    PREPARING_SHIPMENT("배송준비중"),
+    SHIPPING("배송중"),
+    DELIVERED("배송완료"),
+    CANCELED("취소완료"),
+    RETURN_REQUESTED("반품요청"),
+    EXCHANGE_REQUESTED("교환요청"),
+    RETURNED("반품완료"),
+    EXCHANGED("교환완료"),
+    CONFIRMED("구매확정");
+
+    private final String description;
 
     public static OrderStatus of(String orderStatus) {
         return Arrays.stream(OrderStatus.values())
