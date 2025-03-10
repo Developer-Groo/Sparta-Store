d5c6fa74a63cbd01e247d8373b3a062d53f7e4d2
ww313 <wlsghddl3358@naver.com>
2025-03-10 10:20:52 +0900

    selectItem,RedisSortedSet

---
+++ src/main/java/com/example/Sparta_Store/item/dto/response/SelectItemResponseDto.java
@@ -0,0 +1,6 @@
+package com.example.Sparta_Store.item.dto.response;
+
+public record SelectItemResponseDto (
+        
+){
+}
---
+++ src/main/java/com/example/Sparta_Store/ranking/RedisRankingService.java
@@ -0,0 +1,4 @@
+package com.example.Sparta_Store.ranking;
+
+public class RedisRankingService {
+}
