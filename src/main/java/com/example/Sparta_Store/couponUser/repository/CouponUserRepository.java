33ca0a6f543a8a5be4d9bf0970b766ee76c2c3a7
sy <tndus1886@naver.com>
2025-03-11 04:26:00 +0900

    feat: 주문 시에 쿠폰 사용 구현

--- src/main/java/com/example/Sparta_Store/couponUser/repository/CouponUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Sparta_Store.couponUser.repository;
-
-import com.example.Sparta_Store.couponUser.entity.CouponUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CouponUserRepository extends JpaRepository<CouponUser, Long>, CouponUserQueryRepository {
-
-}
