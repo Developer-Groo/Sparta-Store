3a1f0c049be24b43ac98c01cd77c4ae3e02c2c4f
Groo <woohyeon3382@naver.com>
2025-03-12 05:09:31 +0900

    docs: deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 set -e
 
+# Session manager 설치
+sudo yum install -y amazon-ssm-agent
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
+
+# Docker 설치
+sudo yum update -y
+sudo amazon-linux-extras enable docker
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -aG docker ec2-user
+
+# CodeDeploy Agent 설치
+cd /home/ec2-user
+wget https://aws-codedeploy-ap-northeast-2.s3.ap-northeast-2.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
+sudo service codedeploy-agent start
+
 # 환경변수 로드
 ECR_REPO_URI="${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-2.amazonaws.com/sparta-store-repo"
 CONTAINER_NAME="sparta-app"
