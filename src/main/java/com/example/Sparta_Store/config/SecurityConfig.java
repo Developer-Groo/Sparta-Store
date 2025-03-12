3b7a3afba0973f2a3b3f50f3296dbe83942809af
Groo <woohyeon3382@naver.com>
2025-03-13 04:24:56 +0900

    refactor: security config 설정 변경

--- src/main/java/com/example/Sparta_Store/config/SecurityConfig.java
+++ src/main/java/com/example/Sparta_Store/config/SecurityConfig.java
@@ -26,22 +26,23 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(AbstractHttpConfigurer::disable)
-            .httpBasic(AbstractHttpConfigurer::disable)
-            .formLogin(AbstractHttpConfigurer::disable)
-            .addFilterBefore(jwtFilter, SecurityContextHolderAwareRequestFilter.class)
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/users/signUp", "/users/login", "/login", "/css/**", "/oauth2/**", "/auth/login", "/style.css").permitAll() // 회원가입, 로그인, CSS 파일 요청 허용
-                .requestMatchers(HttpMethod.GET, "/items", "/health", "/items/**").permitAll() // 아이템 조회 허용
-                .requestMatchers("/admin/**").hasRole("ADMIN") //admin 이 붙은것은 ADMIN 이 존재해야만 통과 나머지는 누구나 가능하게 했습니다.
-                .anyRequest().authenticated()
-            )
-            .exceptionHandling(exceptionHandling ->
-                    exceptionHandling
-                            .authenticationEntryPoint((request, response, authException) -> {
-                                response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied");
-                            })
-            );
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtFilter, SecurityContextHolderAwareRequestFilter.class)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/users/signUp", "/users/login", "/login", "/css/**", "/oauth2/**", "/auth/login", "/style.css").permitAll() // 회원가입, 로그인, CSS 파일 요청 허용
+                        .requestMatchers(HttpMethod.GET, "/items", "/items/**").permitAll() // 아이템 조회 허용
+                        .requestMatchers(HttpMethod.HEAD, "/health").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN") //admin 이 붙은것은 ADMIN 이 존재해야만 통과 나머지는 누구나 가능하게 했습니다.
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling
+                                .authenticationEntryPoint((request, response, authException) -> {
+                                    response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied");
+                                })
+                );
 
         http.oauth2Login(oauth -> oauth.successHandler(customOAuth2SuccessHandler));
 
