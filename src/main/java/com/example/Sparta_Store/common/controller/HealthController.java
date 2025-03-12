d18edeb10b10a7f7a6ea160001671d43a455dbdf
Groo <woohyeon3382@naver.com>
2025-03-13 06:19:52 +0900

    refactor: health controller 수정

--- src/main/java/com/example/Sparta_Store/common/controller/HealthController.java
+++ src/main/java/com/example/Sparta_Store/common/controller/HealthController.java
@@ -2,13 +2,15 @@ package com.example.Sparta_Store.common.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/health")
 public class HealthController {
 
-    @GetMapping("/health")
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.HEAD})
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.status(HttpStatus.OK)
                 .body("Healthy");
