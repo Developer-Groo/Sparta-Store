030abb73de754f3d484c5f8c17f0492c6e0469c0
Groo <woohyeon3382@naver.com>
2025-03-12 12:10:25 +0900

    docs: 환경변수 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -64,21 +64,14 @@ aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS
 # 최신 이미지 Pull
 docker pull $ECR_REPO_URI:latest
 
+# .env 파일 로드
+if [ -f /home/ubuntu/.env ]; then
+  export $(grep -v '^#' /home/ubuntu/.env | xargs)
+fi
+
 # 기존 컨테이너 종료
 docker stop $CONTAINER_NAME || true
 docker rm $CONTAINER_NAME || true
 
 # 컨테이너 실행
-docker run -d -p $PORT:$PORT --name $CONTAINER_NAME \
-  -e GOOGLE_CLIENT_ID="${GOOGLE_CLIENT_ID}" \
-  -e GOOGLE_CLIENT_SECRET="${GOOGLE_CLIENT_SECRET}" \
-  -e GOOGLE_REDIRECT_URI="${GOOGLE_REDIRECT_URI}" \
-  -e MYSQL_DB_PASSWORD="${MYSQL_DB_PASSWORD}" \
-  -e MYSQL_URL="${MYSQL_URL}" \
-  -e REDIS_HOST="${REDIS_HOST}" \
-  -e NAVER_MAIL_USER_NAME="${NAVER_MAIL_USER_NAME}" \
-  -e NAVER_MAIL_EMAIL_PASSWORD="${NAVER_MAIL_EMAIL_PASSWORD}" \
-  -e TOSS_CLIENT_KEY="${TOSS_CLIENT_KEY}" \
-  -e TOSS_SECRET_KEY="${TOSS_SECRET_KEY}" \
-  -e JWT_SECRET_KEY="${JWT_SECRET_KEY}" \
-  $ECR_REPO_URI:latest
+docker run -d -p $PORT:$PORT --name $CONTAINER_NAME --env-file /home/ubuntu/.env $ECR_REPO_URI:latest
