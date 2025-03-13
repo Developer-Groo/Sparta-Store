109e5c55c662007d2bd951ac2901e0d53a2c4e49
Groo <woohyeon3382@naver.com>
2025-03-13 09:16:43 +0900

    test

--- src/main/java/com/example/Sparta_Store/config/SecurityConfig.java
+++ src/main/java/com/example/Sparta_Store/config/SecurityConfig.java
@@ -1,17 +1,16 @@
 package com.example.Sparta_Store.config;
 
 import com.example.Sparta_Store.oAuth.handler.CustomOAuth2SuccessHandler;
-import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import com.example.Sparta_Store.oAuth.jwt.JwtFilter;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
 
@@ -38,9 +37,6 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.HEAD, "/health").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN") //admin 이 붙은것은 ADMIN 이 존재해야만 통과 나머지는 누구나 가능하게 했습니다.
                         .anyRequest().authenticated()
-                )
-                .headers(headers ->
-                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
                 )
                 .exceptionHandling(exceptionHandling ->
                         exceptionHandling
